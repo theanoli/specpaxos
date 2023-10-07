770da934b3c4a008d332e6b1e005071d6674b573
Matthew Giordano <mgiordan@cs.washington.edu>
2023-10-07 11:31:18 -0700

    change run amt

--- scripts/do_all_runs.sh
+++ scripts/do_all_runs.sh
@@ -4,7 +4,7 @@ CONFIG=${1}
 VERSION_NO=${2}
 
 DEFAULT_THREADS=1
-DEFAULT_NUM_REQS=250000
+DEFAULT_NUM_REQS=300000
 DEFAULT_PAYLOAD_SIZE=256
 DEFAULT_WARMUP_PERIOD=10
 
@@ -13,7 +13,7 @@ set -e
 # iterate over threads
 for threads in 1 2 4 8 16; do
 	# iterate over payload size
-	for payload in 32 64 128 256 512 1024; do
+	for payload in 32 64 128 256 512 900 1024; do
 		./run_wrapper.sh $CONFIG $VERSION_NO $threads $DEFAULT_NUM_REQS $payload $DEFAULT_WARMUP_PERIOD
 	done
 done
