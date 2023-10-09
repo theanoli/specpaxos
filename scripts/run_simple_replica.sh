01346ee76a3f28bf7851bfbad66293305bf3bd44
Matthew Giordano <mgiordan@cs.washington.edu>
2023-10-09 16:54:17 -0700

    add sudo :O

--- scripts/run_simple_replica.sh
+++ scripts/run_simple_replica.sh
@@ -17,7 +17,7 @@ runReplica() {
 
     local config="${CONFIG_DIR}/${config_file}"
 
-    local cmd="sudo taskset -c 0 ${SERVER_BINARY} -c ${config} -m vrw -i ${replica_index}"
+    local cmd="cat ~/specpaxos/scripts/passwd | sudo -S taskset -c 0 ${SERVER_BINARY} -c ${config} -m vrw -i ${replica_index}"
     echo "${cmd}"
     eval "${cmd}"
 }
