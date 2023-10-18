4996f3e03dc5e9c50ffe6af2d2da8a7d37843305
theanoli <theanosaurus@gmail.com>
2023-10-17 18:17:40 -0700

    report last committed after state transfer

--- vrw/replica.cc
+++ vrw/replica.cc
@@ -1134,7 +1134,7 @@ VRWReplica::HandleStateTransfer(const TransportAddress &remote,
         HandlePrepare(*msgpair.first, msgpair.second);
         delete msgpair.first;
     }
-    RNotice("STATETRANSFER completed; new lastCommitted: " FMT_VIEWSTAMP, lastCommitted);
+    RNotice("STATETRANSFER completed; new lastCommitted: " FMT_OPNUM, lastCommitted);
 }
 
 void
