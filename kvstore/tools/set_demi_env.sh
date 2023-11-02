a44f76afe1687862feff37458612c9060f8ce7af
HelloKayT <katiemlim@gmail.com>
2023-11-02 00:26:26 -0700

    Fix some parameters for jumbo packets

--- kvstore/tools/set_demi_env.sh
+++ kvstore/tools/set_demi_env.sh
@@ -1,7 +1,7 @@
 export CONFIG_PATH=/home/thst/config.yaml
 export LIBOS=catnip
-export MSS=1500
-export MTU=1500
+export MSS=8800
+export MTU=9100
 export TCP_CHECKSUM_OFFLOAD=1
 export UDP_CHECKSUM_OFFLOAD=1
 export USE_JUMBO=1
