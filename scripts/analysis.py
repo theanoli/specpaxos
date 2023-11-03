b5534777ed67781ab835b088558288fc3937b780
Matthew Giordano <mgiordan@cs.washington.edu>
2023-11-03 11:30:06 -0700

    more graph

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -9,14 +9,8 @@ import os, sys, mmap
 import re
 from collections import defaultdict
 
-def main():
-    config = sys.argv[1]
-    version = sys.argv[2]
-    #num_threads = sys.argv[3]
-    time_ran = sys.argv[3]
-    #payload_size = sys.argv[5]
-    warmup_period = sys.argv[4]
-
+def analyze(config, version, time_ran, warmup_period, output=True):
+    
     fio = FileIO('logs,{config},{version}/{num_threads},{time_ran},{payload_size},{warmup_period},{thread_num},{run_num},{suffix}')
 
     df, meta = fio.get_raw_data({'config': config, 'version': version, 'time_ran': time_ran, 'warmup_period': warmup_period}, lambda path: {"raw data": list(map(int, Path(path).read_text().strip().split("\n")))})
@@ -36,21 +30,33 @@ def main():
 
     # if there are any duplicate runs
     if any(df['run_num'] == 1):
-        graph(df, meta, 'num_threads', 'total thruput', 'payload size + run number',
-                        'Num Clients', 'Throughput (MB/s)', 'Payload Size.Run Number')
+        if output:
+            graph(df, meta, 'num_threads', 'total thruput', 'payload size + run number',
+                            'Num Clients', 'Throughput (MB/s)', 'Payload Size.Run Number')
 
     df, meta = collapse(df, meta, ['run_num'], 
             {'average runtime': ('total runtime', 'mean'),
              'average thruput': ('total thruput', 'mean'),
              'average ops|sec': ('total ops|sec', 'mean')})
 
-    print(df.head())
+    if output:
+        print(df.head())
 
-    graph(df, meta, 'num_threads', 'average thruput', 'payload_size',
-                    'Num Clients', 'Throughput (MB/s)', 'Payload Size (B)')
+        graph(df, meta, 'num_threads', 'average thruput', 'payload_size',
+                        'Num Clients', 'Throughput (MB/s)', 'Payload Size (B)')
 
-    graph(df, meta, 'num_threads', 'average ops|sec', 'payload_size',
-                    'Num Clients', 'Operations/sec (ops/s)', 'Payload Size (B)')
+        graph(df, meta, 'num_threads', 'average ops|sec', 'payload_size',
+                        'Num Clients', 'Operations/sec (ops/s)', 'Payload Size (B)')
+    return df, meta
+
+def main():
+    config = sys.argv[1]
+    version = sys.argv[2]
+    #num_threads = sys.argv[3]
+    time_ran = sys.argv[3]
+    #payload_size = sys.argv[5]
+    warmup_period = sys.argv[4]
+    analyze(config, version, time_ran, warmup_period)
 
 if __name__ == '__main__':
     main()
---
+++ scripts/tripleanalysis.py
@@ -0,0 +1,53 @@
+from analysis import main as process
+
+import os, sys, mmap
+sys.path.append(os.getcwd() + '/../../scripts')
+from gengraph import graph
+from fileio import FileIO, collapse
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+import re
+from collections import defaultdict
+
+from analysis import analyze
+def main():
+    config1 = sys.argv[1]
+    version1 = sys.argv[2]
+    config2 = sys.argv[3]
+    version2 = sys.argv[4]
+    config3 = sys.argv[5]
+    version3 = sys.argv[6]
+
+    TIME_RAN = 30000000000
+    WARMUP_TIME = 10
+
+    output = False
+
+    df1, m1 = analyze(config1, version1, TIME_RAN, WARMUP_TIME, output=output)
+    df2, m2 = analyze(config2, version2, TIME_RAN, WARMUP_TIME, output=output)
+    df3, m3 = analyze(config3, version3, TIME_RAN, WARMUP_TIME, output=output)
+
+    options = [32, 1024]
+    df1 = df1.loc[df1['payload_size'].isin(options)]
+    df2 = df2.loc[df2['payload_size'].isin(options)]
+    df3 = df3.loc[df3['payload_size'].isin(options)]
+
+    df1["source"] = "CPU Beehive Off"
+    df2["source"] = "CPU Beehive On"
+    df3["source"] = "FPGA"
+
+    df1["source + payload size"] = df1["source"] + " - " + df1["payload_size"].apply(str)
+    df2["source + payload size"] = df2["source"] + " - " + df2["payload_size"].apply(str)
+    df3["source + payload size"] = df3["source"] + " - " + df3["payload_size"].apply(str)
+
+    df = pd.concat([df1, df2, df3]).reset_index(drop=True)
+
+    graph(df, m1,   'num_threads', 'average ops|sec', 'source + payload size', 
+                    'Num Clients', 'Operations/sec (ops/s)', 'Payload Size (B)', sortX=False)
+
+
+if __name__ == '__main__':
+    main()
