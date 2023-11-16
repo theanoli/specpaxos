33291265251ee0c39f08f74d0cc1edac94685c45
HelloKayT <katiemlim@gmail.com>
2023-11-16 09:25:50 -0800

    Add ability to put IRQs back on both sockets

--- kvstore/tools/set_irqs.sh
+++ kvstore/tools/set_irqs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then 
-    echo "Usage: set_irqs <devname>"
+if [ $# -ne 2 ]; then 
+    echo "Usage: set_irqs <devname> <num_numa_nodes>"
 fi
 
 DEV_NAME=$1
+NUM_NUMA_NODES=$2
 
 IRQS=$(cat /proc/interrupts | grep ${DEV_NAME} | cut -d ":" -f1)
 
@@ -11,7 +12,20 @@ bitshift=0
 lower=1
 for IRQ in ${IRQS[@]}; do
     irq_mask=$((1 << $bitshift))
-    bitmask=$(printf "%08x" ${irq_mask})
+
+    bitmask=""
+    if [ ${NUM_NUMA_NODES} -eq 1 ]; then
+        bitmask=$(printf "%08x" ${irq_mask})
+    elif [ ${NUM_NUMA_NODES} -eq 2 ]; then
+        if [ ${lower} -eq 1 ]; then
+            bitmask=$(printf "%04x0000" ${irq_mask})
+        else
+            bitmask=$(printf "%08x" ${irq_mask})
+        fi
+    else
+        echo "Unsupported number of NUMA nodes ${NUM_NUMA_NODES}"
+        exit 1
+    fi
    
     cpu_mask=""
     if [ ${lower} -eq 1 ]; then
