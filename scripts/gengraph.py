bd2428da05ba79b0bc622889fabaaf0f2d0dfc85
Matthew Giordano <mgiordan@cs.washington.edu>
2023-11-20 13:48:46 -0800

    add paper ready graph :)

--- scripts/gengraph.py
+++ scripts/gengraph.py
@@ -15,7 +15,7 @@ import re
 from collections import defaultdict
 
 
-def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title='', symbolProp=None, sortX=True, connectZ=False, left=0, right=None, bottom=0, extra_title="", xscale="linear", yscale="linear", markersize=None):
+def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title='', symbolProp=None, sortX=True, left=0, right=None, bottom=0, extra_title="", xscale="linear", yscale="linear", markersize=None, legendLoc="best"):
     # Assumption: all datas have the same config and version for one graph() call
     # get all the (x, y, z, error) pairs
     #data4 = [(run[xprop], run[yprop], zprop_lambda(run), run['error']) for run in data]
@@ -26,13 +26,13 @@ def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title=
         zlabel = None
         df[zprop] = '0'
     #print('z props:', df[zprop].unique())
-    zlayers = sorted(df[zprop].unique())
+    zlayers = df[zprop].unique()
     fig, ax = plt.subplots()
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    colors = list(mcolors.TABLEAU_COLORS)
+    colors = list(mcolors.TABLEAU_COLORS) + list(mcolors.XKCD_COLORS)
     dashings = ["-", "--"]
-    symbols = ["o", "^", "v", "<", ">", "D", "s"]
+    symbols = ["o", "^", "v", "<", ">", "D", "s", "8", "*", "d"]
     zI = 0
     sI = 0
     symbolValue = None
@@ -55,7 +55,7 @@ def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title=
             if symbolValue is None:
                 symbolValue = sV[0]
             elif sV[0] != symbolValue:
-                #print(f"mismatch in {sV[0]} and {symbolValue}")
+                print(f"mismatch in {sV[0]} and {symbolValue}")
                 symbolValue = sV[0]
                 zI += 1
                 sI = 0
@@ -65,7 +65,7 @@ def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title=
             symbol = symbols[zI]
         else:
             color = colors[zI]
-            symbol = symbols[zI]
+            symbol = symbols[zI] # potato
             zI += 1
         p, = ax.plot(xs, ys,  symbol + dashings[sI],c=color, markersize=markersize)
         for (x, y, e) in zip(xs, ys, es):
@@ -85,7 +85,7 @@ def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title=
     ax.set_xscale(xscale)
     ax.set_yscale(yscale)
     if zlabel is not None:
-        ax.legend(title=zlabel)
+        ax.legend(title=zlabel, loc=legendLoc)
     # Assumption: all datas have the same config and version for one graph() call
     row0 = df.loc[0]
     if title == '':
@@ -100,7 +100,8 @@ def graph(df, meta, xprop, yprop, zprop, xlabel='', ylabel='', zlabel='', title=
             savename += f"{item},"
 
     savename += f"{xprop} vs {yprop} vs {zprop},"
-    savename += extra_title + ","
+    if len(extra_title) != 0:
+        savename += extra_title + ","
     #plt.show()
     plt.savefig("graphs/" + savename + ".png")
     plt.savefig("pdfs/" + savename + ".pdf")
